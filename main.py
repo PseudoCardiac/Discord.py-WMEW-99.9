a5b8f1b644bb372c97254e3a04e6c3fd0a723bb2
42 <nooyeahc1125@gmail.com>
2026-06-22 10:45:41 +0900

    vbr 키워드 삭제

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ async def playAudio():
 
         for curr in radio:
             src, time = curr
-            VC.play( discord.FFmpegOpusAudio( executable = FFMPEG_PATH , source = src, options = "-vbr on" ) )
+            VC.play( discord.FFmpegOpusAudio( executable = FFMPEG_PATH , source = src ) )
             await asyncio.sleep( time )
             while VC.is_playing():
                 await asyncio.sleep( 0.1 )
